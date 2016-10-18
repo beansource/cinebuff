ff07027a4419470d6144c583a74e80ca53389274
Eric Carboni <ericcarboni@gmail.com>
2016-10-18 18:47:24 -0400

    Implemented half-ass version of AutoComplete search

---
+++ app/src/main/java/com/carboni/cinebuff/AutoCompleteAdapter.java
@@ -0,0 +1,148 @@
+package com.carboni.cinebuff;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.LoginFilter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.carboni.cinebuff.model.Person;
+import com.carboni.cinebuff.model.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by ericcarboni on 10/16/16.
+ */
+
+public class AutoCompleteAdapter extends BaseAdapter implements Filterable {
+    public static List<Result> resultList;
+    public static List<Result> fromApi;
+    private Context mContext;
+
+    public AutoCompleteAdapter(Context context) {
+        mContext = context;
+        resultList = new ArrayList<Result>();
+        fromApi = new ArrayList<Result>();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.i("TAG", "getView() called");
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.auto_complete_item, parent, false);
+        }
+        ((TextView) convertView.findViewById(R.id.autoCompleteName)).setText(getItem(position).getName());
+        // ((TextView) convertView.findViewById(R.id.autoCompleteId)).setText(getItem(position).getId());
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        final Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint != null) {
+                    // resultList.clear();
+                    Log.i("TAG", "performFiltering() with " + constraint.toString());
+                    // constraint.toString() is query from user
+                    resultList = getApi(constraint.toString());
+                    filterResults.values = resultList;
+                    filterResults.count = resultList.size();
+                    // Log.i("performFiltering()", "Count: " + filterResults.count + "\nNames: " + filterResults.values.toString());
+                    notifyDataSetChanged();
+                    // return filterResults;
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results != null && results.count > 0) {
+                    // Log.i("TAG", "clear() called");
+                    // resultList.clear();
+
+                    /*resultList = (List<Result>) results.values;
+                    for (Result name : resultList) {
+                        resultList.add(name);
+                        Log.i("publishResults()", name.getName());
+                    }*/
+
+                    notifyDataSetChanged();
+                } else {
+                    Log.i("TAG", "Results null or count < 0");
+                    notifyDataSetInvalidated();
+                }
+            }
+        };
+        return filter;
+    }
+
+    public List<Result> getApi(String constraint) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        TMdbAPI tMdbAPI = retrofit.create(TMdbAPI.class);
+        Call<Person> call = tMdbAPI.searchPerson(constraint.toString());
+        call.enqueue(new Callback<Person>() {
+            @Override
+            public void onResponse(Call<Person> call, Response<Person> response) {
+                List<Result> names = response.body().getResults();
+                for (Result name : names) {
+                    Log.i("TAG", "Name: " + name.getName());
+                    fromApi.add(name);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Person> call, Throwable t) {
+
+            }
+        });
+        return fromApi;
+    }
+
+    @Override
+    public int getCount() {
+        Log.i("TAG", "getCount() returns " + resultList.size());
+        return resultList.size();
+    }
+
+    @Nullable
+    @Override
+    public Result getItem(int position) {
+        return resultList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+        Log.i("FOO", "Item count: " + resultList.size());
+    }
+}
