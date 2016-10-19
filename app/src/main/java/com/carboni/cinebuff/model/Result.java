52c566a56093b8740fdb064bc518cbd3025d8132
Eric Carboni <ericcarboni@gmail.com>
2016-10-19 17:49:18 -0400

    Don't need setter methods

--- app/src/main/java/com/carboni/cinebuff/model/Result.java
+++ app/src/main/java/com/carboni/cinebuff/model/Result.java
@@ -27,24 +27,10 @@ public class Result {
     }
 
     /**
-     * @param profilePath The profile_path
-     */
-    public void setProfilePath(String profilePath) {
-        this.profilePath = profilePath;
-    }
-
-    /**
      * @return The adult
      */
     public Boolean getAdult() {
         return adult;
-    }
-
-    /**
-     * @param adult The adult
-     */
-    public void setAdult(Boolean adult) {
-        this.adult = adult;
     }
 
     /**
@@ -55,24 +41,10 @@ public class Result {
     }
 
     /**
-     * @param id The id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
      * @return The knownFor
      */
     public List<KnownFor> getKnownFor() {
         return knownFor;
-    }
-
-    /**
-     * @param knownFor The known_for
-     */
-    public void setKnownFor(List<KnownFor> knownFor) {
-        this.knownFor = knownFor;
     }
 
     /**
@@ -83,32 +55,15 @@ public class Result {
     }
 
     /**
-     * @param name The name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
      * @return The popularity
      */
     public Double getPopularity() {
         return popularity;
     }
 
-    /**
-     * @param popularity The popularity
-     */
-    public void setPopularity(Double popularity) {
-        this.popularity = popularity;
-    }
 
     public Map<String, Object> getAdditionalProperties() {
         return this.additionalProperties;
     }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
+    
 }
