30e09b2a13569db9b16e26826ae389ca766436e9
Eric Carboni <ericcarboni@gmail.com>
2016-10-25 16:36:33 -0400

    Dependency Injection

---
+++ app/src/main/java/com/carboni/cinebuff/ActivityScope.java
@@ -0,0 +1,12 @@
+package com.carboni.cinebuff;
+
+import javax.inject.Scope;
+
+/**
+ * Created by ericcarboni on 10/25/16.
+ */
+
+@Scope
+public @interface ActivityScope {
+
+}
---
+++ app/src/main/java/com/carboni/cinebuff/AppComponent.java
@@ -0,0 +1,16 @@
+package com.carboni.cinebuff;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by ericcarboni on 10/25/16.
+ */
+@Singleton
+@Component(modules = {
+
+})
+
+public interface AppComponent {
+}
