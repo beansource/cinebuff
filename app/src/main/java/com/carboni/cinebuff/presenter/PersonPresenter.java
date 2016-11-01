8c76c4f2f5b5d8c6cc451f0bb336deecd9a65905
Eric Carboni <ericcarboni@gmail.com>
2016-11-01 14:33:52 -0400

    onNetworkFailure now passes a Throwable

--- app/src/main/java/com/carboni/cinebuff/presenter/PersonPresenter.java
+++ app/src/main/java/com/carboni/cinebuff/presenter/PersonPresenter.java
@@ -1,5 +1,7 @@
 package com.carboni.cinebuff.presenter;
 
+import android.util.Log;
+
 import com.carboni.cinebuff.OnPersonInteractorFinishedListener;
 import com.carboni.cinebuff.interactors.SearchPersonInteractor;
 import com.carboni.cinebuff.model.Result;
@@ -8,6 +10,7 @@ import com.carboni.cinebuff.view.PersonView;
 import java.util.List;
 
 import retrofit2.Response;
+import retrofit2.Retrofit;
 
 /**
  * Created by ericcarboni on 10/24/16.
@@ -20,20 +23,24 @@ public class PersonPresenter implements Presenter, OnPersonInteractorFinishedLis
     public PersonPresenter(PersonView view) {
         this.view = view;
         this.interactor = new SearchPersonInteractor(this);
+        Log.i("PersonPresenter", "PersonPresenter constructor called");
     }
 
     @Override
     public void attemptSearch(String query) {
+        Log.i("PersonPresenter", "PersonPresenter attemptSearch() called");
         interactor.loadPerson(query);
     }
 
     @Override
     public void onNetworkSuccess(List<Result> list, Response response) {
+        Log.i("PersonPresenter", "PersonPresenter got success from Interactor");
         view.showSuccess(list, response);
     }
 
     @Override
-    public void onNetworkFailure() {
-        view.showFailure();
+    public void onNetworkFailure(Throwable error) {
+        Log.i("PersonPresenter", "PersonPresenter got failure from Interactor");
+        view.showFailure(error);
     }
 }
