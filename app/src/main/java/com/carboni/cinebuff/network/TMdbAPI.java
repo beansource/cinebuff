93bade3950c0d9249e8122a893e30c846ccc03b1
Eric Carboni <ericcarboni@gmail.com>
2016-11-11 00:38:22 -0500

    Added comments

--- app/src/main/java/com/carboni/cinebuff/network/TMdbAPI.java
+++ app/src/main/java/com/carboni/cinebuff/network/TMdbAPI.java
@@ -14,9 +14,15 @@ import retrofit2.http.Query;
 public interface TMdbAPI {
     String API_KEY = BuildConfig.TMDB_API_KEY;
 
+    /*
+    Retrieve list of People
+     */
     @GET("search/person?api_key=" + API_KEY)
     Call<Person> searchPerson(@Query("query") String query);
 
+    /*
+    Retrieve list of Movies
+     */
     @GET("discover/movie?api_key=" + API_KEY)
     Call getMovies(@Query("with_people") String query);
 
