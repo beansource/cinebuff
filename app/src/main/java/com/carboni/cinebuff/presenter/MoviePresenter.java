12e41a0c47fd3e77b8345891e502b0c184195da5
Eric Carboni <ericcarboni@gmail.com>
2016-11-19 16:01:07 -0500

    Add constructor and implement base presenter

--- app/src/main/java/com/carboni/cinebuff/presenter/MoviePresenter.java
+++ app/src/main/java/com/carboni/cinebuff/presenter/MoviePresenter.java
@@ -1,8 +1,20 @@
 package com.carboni.cinebuff.presenter;
 
+import com.carboni.cinebuff.view.MoviesView;
+
 /**
  * Created by ericcarboni on 10/24/16.
  */
 
-public class MoviePresenter {
+public class MoviePresenter implements Presenter {
+    public MoviesView view;
+
+    public MoviePresenter(MoviesView view) {
+        this.view = view;
+    }
+
+    @Override
+    public void attemptSearch(String query) {
+
+    }
 }
