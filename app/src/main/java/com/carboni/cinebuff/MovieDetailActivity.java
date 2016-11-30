8ad424cbee0c31bb6deee7963fe2b8a590609d23
Eric Carboni <ericcarboni@gmail.com>
2016-11-30 11:29:27 -0500

    Implement horizontal recycler view to display list of Cast members

--- app/src/main/java/com/carboni/cinebuff/MovieDetailActivity.java
+++ app/src/main/java/com/carboni/cinebuff/MovieDetailActivity.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.carboni.cinebuff.adapter.CastListAdapter;
 import com.carboni.cinebuff.model.Cast;
 import com.carboni.cinebuff.model.Crew;
 import com.carboni.cinebuff.model.Genre;
@@ -42,8 +46,15 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
     TextView movie_genres;
     @BindView(R.id.movie_detail_director)
     TextView movie_director;
+    @BindView(R.id.movie_detail_writer)
+    TextView movie_writer;
     @BindView(R.id.movie_detail_loading)
     ProgressBar loading;
+    @BindView(R.id.movie_detail_cast)
+    RecyclerView cast_recycler_view;
+
+    private RecyclerView.Adapter castAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     MovieDetailPresenter presenter;
 
@@ -53,14 +64,16 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
         setContentView(R.layout.movie_detail);
         ButterKnife.bind(this);
 
-        presenter = new MovieDetailPresenter(this);
-
         Intent intent = getIntent();
         String title = intent.getStringExtra("MOVIE_TITLE");
         String image_url = intent.getStringExtra("MOVIE_IMAGE_URL");
         int movie_id = intent.getIntExtra("MOVIE_ID", 0);
 
+        presenter = new MovieDetailPresenter(this);
         presenter.attemptSearch(movie_id + "");
+
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        cast_recycler_view.setLayoutManager(layoutManager);
 
         toolbar.setTitle(title);
         setSupportActionBar(toolbar);
@@ -80,9 +93,6 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
         MovieCredits credits = o.credits;
         List<Cast> cast = credits.getCast();
         List<Crew> crew = credits.getCrew();
-
-        // this is where I would set adapters for the list of cast and crew
-
         List<Genre> genres = movie.getGenres();
         String all_genres = "";
         for (Genre genre : genres) {
@@ -94,13 +104,21 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
         movie_genres.setText(all_genres);
 
         // TODO: This is terrible, we can figure out a better way to get director
-        String dir = "";
+        String dir = "", writ = "";
         for (Crew member : crew) {
             if (member.getJob().toString().equals("Director")) {
                 dir = member.getName();
             }
+            if (member.getJob().toString().equals("Screenplay") || member.getJob().toString().equals("Writer")) {
+                writ = member.getName();
+            }
         }
-        movie_director.setText("DIRECTOR: " + dir);
+        movie_director.setText("DIRECTOR: " + dir); // Again, terrible practice
+        movie_writer.setText("WRITER: " + writ);
+
+        // Cast list recycler view
+        castAdapter = new CastListAdapter(cast, this); //TODO : Set up click listener
+        cast_recycler_view.setAdapter(castAdapter);
 
         loading.setVisibility(View.GONE);
         details_view.setVisibility(View.VISIBLE); // TODO: Animate this view in from bottom (See Plaid)
--- app/src/main/java/com/carboni/cinebuff/adapter/CastListAdapter.java
+++ app/src/main/java/com/carboni/cinebuff/adapter/CastListAdapter.java
@@ -1,9 +1,16 @@
 package com.carboni.cinebuff.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.carboni.cinebuff.CircleTransformation;
+import com.carboni.cinebuff.R;
 import com.carboni.cinebuff.model.Cast;
 
 import java.util.List;
@@ -14,27 +21,48 @@ import java.util.List;
 
 public class CastListAdapter extends RecyclerView.Adapter<CastListAdapter.ViewHolder> {
     private List<Cast> list;
+    Context context;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView castImage;
+        public TextView castName;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            castImage = (ImageView) itemView.findViewById(R.id.movie_detail_cast_image_view);
+            castName = (TextView) itemView.findViewById(R.id.movie_detail_cast_name);
         }
+    }
+
+    public CastListAdapter(List<Cast> list, Context context) {
+        this.list = list;
+        this.context = context;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.movie_detail_cast_list_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        final Cast castMember = list.get(position);
+        holder.castName.setText(castMember.getName());
+        Glide
+                .with(context)
+                .load("https://image.tmdb.org/t/p/w500" + castMember.getProfilePath())
+                .placeholder(R.mipmap.ic_person_placeholder)
+                .crossFade()
+                .centerCrop()
+                .transform(new CircleTransformation(context))
+                .into(holder.castImage);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
-
 
 }
