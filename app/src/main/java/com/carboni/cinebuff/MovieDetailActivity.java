6a87701d4d7cd95b2fc442fd92b269bf88fcff74
Eric Carboni <ericcarboni@gmail.com>
2016-11-29 23:17:48 -0500

    Pull director into detail page

--- app/src/main/java/com/carboni/cinebuff/MovieDetailActivity.java
+++ app/src/main/java/com/carboni/cinebuff/MovieDetailActivity.java
@@ -2,6 +2,7 @@ package com.carboni.cinebuff;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.carboni.cinebuff.model.Cast;
+import com.carboni.cinebuff.model.Crew;
 import com.carboni.cinebuff.model.Genre;
 import com.carboni.cinebuff.model.MovieCredits;
 import com.carboni.cinebuff.model.MovieDetail;
@@ -28,12 +31,16 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
     Toolbar toolbar;
     @BindView(R.id.movie_backdrop)
     ImageView backdrop;
+    @BindView(R.id.constraint_layout)
+    ConstraintLayout details_view;
     @BindView(R.id.movie_detail_summary)
     TextView movie_summary;
     @BindView(R.id.movie_detail_tagline)
     TextView movie_tagline;
     @BindView(R.id.movie_detail_genres)
     TextView movie_genres;
+    @BindView(R.id.movie_detail_director)
+    TextView movie_director;
     @BindView(R.id.movie_detail_loading)
     ProgressBar loading;
 
@@ -68,6 +75,10 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
     public void showSuccess(MovieDetailAndCredits o) {
         MovieDetail movie = o.detail;
         MovieCredits credits = o.credits;
+        List<Cast> cast = credits.getCast();
+        List<Crew> crew = credits.getCrew();
+
+        // this is where I would set adapters for the list of cast and crew
 
         List<Genre> genres = movie.getGenres();
         String all_genres = "";
@@ -79,7 +90,17 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
         movie_tagline.setText(movie.getTagline());
         movie_genres.setText(all_genres);
 
+        // TODO: This is terrible, we can figure out a better way to get director
+        String dir = "";
+        for (Crew member : crew) {
+            if (member.getJob().toString().equals("Director")) {
+                dir = member.getName();
+            }
+        }
+        movie_director.setText("DIRECTOR: " + dir);
+
         loading.setVisibility(View.GONE);
+        details_view.setVisibility(View.VISIBLE); // TODO: Animate this view in from bottom (See Plaid)
     }
 
     @Override
