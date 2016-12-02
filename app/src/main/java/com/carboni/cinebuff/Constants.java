6c581f6291f9bbda9e535bb0b336137c4cf35969
Eric Carboni <ericcarboni@gmail.com>
2016-12-02 17:24:20 -0500

    Add base API urls helper

---
+++ app/src/main/java/com/carboni/cinebuff/Constants.java
@@ -0,0 +1,12 @@
+package com.carboni.cinebuff;
+
+/**
+ * Created by ericcarboni on 12/2/16.
+ */
+
+public class Constants {
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+
+    public static final String IMAGE_SMALL = "https://image.tmdb.org/t/p/w500";
+    public static final String IMAGE_LARGE = "https://image.tmdb.org/t/p/w1280";
+}
