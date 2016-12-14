cd5dc5bbf2aacd36d6e710eb65b3f26a6f7b50d4
Eric Carboni <ericcarboni@gmail.com>
2016-12-14 01:00:31 -0500

    Animate the expand/collapse icon

--- app/src/main/java/com/carboni/cinebuff/adapter/crew/DepartmentViewHolder.java
+++ app/src/main/java/com/carboni/cinebuff/adapter/crew/DepartmentViewHolder.java
@@ -2,6 +2,7 @@ package com.carboni.cinebuff.adapter.crew;
 
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bignerdranch.expandablerecyclerview.ViewHolder.ParentViewHolder;
@@ -14,10 +15,25 @@ import com.carboni.cinebuff.R;
 public class DepartmentViewHolder extends ParentViewHolder {
     @NonNull
     private TextView departmentText;
+    private ImageView expandCollapseImageView;
 
-    public DepartmentViewHolder(@NonNull View itemView) {
+    private static final float INITIAL_POSITION = 0.0f;
+    private static final float ROTATED_POSITION = 180f;
+
+    @Override
+    public void onExpansionToggled(boolean expanded) {
+        super.onExpansionToggled(expanded);
+        if (expanded) {
+            expandCollapseImageView.animate().rotation(INITIAL_POSITION).start();
+        } else {
+            expandCollapseImageView.animate().rotation(ROTATED_POSITION).start();
+        }
+    }
+
+    public DepartmentViewHolder(@NonNull final View itemView) {
         super(itemView);
         departmentText = (TextView) itemView.findViewById(R.id.crew_department_textview);
+        expandCollapseImageView = (ImageView) itemView.findViewById(R.id.crew_department_expand_collapse_view);
     }
 
     public void bind(@NonNull Department department) {
